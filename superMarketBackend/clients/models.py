aa3797966c1d73077b7ad853bdce60dcc8e161e8
EmileNapon <emilenapon0@gmail.com>
2025-02-11 08:44:48 +0000

    first commit

--- superMarketBackend/clients/models.py
+++ superMarketBackend/clients/models.py
@@ -7,7 +7,7 @@ class Client(TenantMixin):
     created_on = models.DateField(auto_now_add=True)  # Date d'inscription
     paid_until = models.DateField(null=True, blank=True)  # Abonnement payé jusqu'à
     on_trial = models.BooleanField(default=True)  # Supermarché en essai ?
-    auto_create_schema = True  # Crée automatiquement le schéma
+    auto_create_schema =models.BooleanField(default=True)  # Crée automatiquement le schéma
     
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
@@ -20,6 +20,4 @@ class Client(TenantMixin):
 
 class Domain(DomainMixin):
     pass
-
-
 
